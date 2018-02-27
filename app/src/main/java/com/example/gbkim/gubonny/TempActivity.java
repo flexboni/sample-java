4cd6a060eaf948e3bef58b570003ca417c360178
gubonny <kgb8594@gmail.com>
2018-02-27 17:43:15 +0900

    DatePicker and TimePicker

--- app/src/main/java/com/example/gbkim/gubonny/TempActivity.java
+++ app/src/main/java/com/example/gbkim/gubonny/TempActivity.java
@@ -5,11 +5,15 @@ import android.app.Dialog;
 import android.app.TimePickerDialog;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.TimePicker;
 import android.widget.Toast;
+
+import java.util.Calendar;
+import java.util.Date;
 
 public class TempActivity extends AppCompatActivity {
 
@@ -18,6 +22,8 @@ public class TempActivity extends AppCompatActivity {
 
     final int DIALOG_DATE = 1;
     final int DIALOG_TIME = 2;
+
+    private String TAG = "PickerActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,13 +46,55 @@ public class TempActivity extends AppCompatActivity {
                 showDialog(DIALOG_TIME);
             }
         });
+
+        init();
+    }
+
+    private void init() {
+        final Calendar cal = Calendar.getInstance();
+
+        Log.e(TAG, cal.get(Calendar.YEAR)+"");
+        Log.e(TAG, cal.get(Calendar.MONTH)+1+"");
+        Log.e(TAG, cal.get(Calendar.DATE)+"");
+        Log.e(TAG, cal.get(Calendar.HOUR_OF_DAY)+"");
+        Log.e(TAG, cal.get(Calendar.MINUTE)+"");
+
+        //DATE PICKER DIALOG
+      btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                DatePickerDialog dialog = new DatePickerDialog(TempActivity.this, new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker datePicker, int year, int month, int date) {
+
+                        String msg = String.format("%d 년 %d 월 %d 일", year, month+1, date);
+                        Toast.makeText(TempActivity.this, msg, Toast.LENGTH_SHORT).show();
+                    }
+                }, cal.get(Calendar.YEAR), cal.get(Calendar.MONTH), cal.get(Calendar.DATE));
+
+                dialog.getDatePicker().setMaxDate(new Date().getTime());    //입력한 날짜 이후로 클릭 안되게 옵션
+                dialog.show();
+
+            }
+        });
+
+      btn2.setOnClickListener(new View.OnClickListener() {
+          @Override
+          public void onClick(View v) {
+
+          }
+      });
+
+
+
     }
 
     @Override
     protected Dialog onCreateDialog(int id) {
         switch (id) {
             case DIALOG_DATE :
-                DatePickerDialog dpd = new DatePickerDialog(getApplicationContext(), new DatePickerDialog.OnDateSetListener() {
+                DatePickerDialog dpd = new DatePickerDialog(TempActivity.this, new DatePickerDialog.OnDateSetListener() {
                     @Override
                     public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
                         Toast.makeText(getApplicationContext(), year+"년 "+(month+1)+"월 "+dayOfMonth+"일 을 선택했습니다", Toast.LENGTH_SHORT).show();
@@ -54,7 +102,7 @@ public class TempActivity extends AppCompatActivity {
                 }, 2000, 1, 1);
                 return dpd;
             case DIALOG_TIME:
-                TimePickerDialog tpd = new TimePickerDialog(getApplicationContext(), new TimePickerDialog.OnTimeSetListener() {
+                TimePickerDialog tpd = new TimePickerDialog(TempActivity.this, new TimePickerDialog.OnTimeSetListener() {
                     @Override
                     public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
                         Toast.makeText(getApplicationContext(), hourOfDay + "시 " + minute + "분 을 선택했습니다.", Toast.LENGTH_SHORT).show();
