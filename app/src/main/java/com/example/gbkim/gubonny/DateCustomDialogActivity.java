d20d40916991e337f78c285617168c15a9c63071
gbkim <kgb8594@gmail.com>
2018-03-02 15:56:59 +0900

    Making NumberPicker

--- app/src/main/java/com/example/gbkim/gubonny/DateCustomDialogActivity.java
+++ app/src/main/java/com/example/gbkim/gubonny/DateCustomDialogActivity.java
@@ -5,30 +5,27 @@ import android.app.Dialog;
 import android.app.TimePickerDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.Button;
 import android.widget.DatePicker;
+import android.widget.NumberPicker;
 import android.widget.TextView;
 import android.widget.TimePicker;
-import android.widget.Toast;
 
-import com.example.gbkim.gubonny.Adapter.Adapter_DateCustomDialog;
-import com.example.gbkim.gubonny.Adapter.RecyclerViewAdapter;
-import com.example.gbkim.gubonny.Listener.RecyclerItemClickListener;
-
-import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
 
-public class DateCustomDialogActivity extends AppCompatActivity {
+public class DateCustomDialogActivity extends AppCompatActivity implements NumberPicker.OnValueChangeListener {
 
-    private RecyclerView rv_dcd;
-    private LinearLayoutManager RecyclerViewLayoutManager;
-    private ArrayList<String> title;
     private Button btn_date;
     private Button btn_time;
+    private Button btn_bp;
+    private Button btn_hp;
+    private Button btn_mp;
+    private Button btn_save;
+    private TextView tv_bp;
+    private TextView tv_hp;
+    private TextView tv_mp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,25 +34,16 @@ public class DateCustomDialogActivity extends AppCompatActivity {
 
         btn_date = findViewById(R.id.btn_date);
         btn_time = findViewById(R.id.btn_time);
+        btn_bp = findViewById(R.id.btn_bp);
+        btn_hp = findViewById(R.id.btn_hp);
+        btn_mp = findViewById(R.id.btn_mp);
+        btn_save = findViewById(R.id.btn_save);
 
-        rv_dcd = findViewById(R.id.rv_dcd);
-        RecyclerViewLayoutManager = new LinearLayoutManager(getApplicationContext());
-        rv_dcd.setLayoutManager(RecyclerViewLayoutManager);
-
-        addItems(); // 카드뷰에 데이터 넣기
+        tv_bp = findViewById(R.id.tv_bp);
+        tv_hp = findViewById(R.id.tv_hp);
+        tv_mp = findViewById(R.id.tv_mp);
 
         event(); // 각종 이벤트
-
-        Adapter_DateCustomDialog adapter = new Adapter_DateCustomDialog(getApplicationContext(), title);
-        rv_dcd.setAdapter(adapter);
-    }
-
-    private void addItems() {
-        title = new ArrayList<>();
-
-        title.add("BP");
-        title.add("HP");
-        title.add("MP");
     }
 
     private void event() {
@@ -67,7 +55,7 @@ public class DateCustomDialogActivity extends AppCompatActivity {
                 DatePickerDialog dpd = new DatePickerDialog(DateCustomDialogActivity.this, new DatePickerDialog.OnDateSetListener() {
                     @Override
                     public void onDateSet(DatePicker datePicker, int year, int month, int date) {
-                        String msg = String.format("%d-%d-%d", year, month+1, date);
+                        String msg = String.format("%d-%d-%d", year, month + 1, date);
                         btn_date.setText(msg);
                     }
                 }, cal.get(Calendar.YEAR), cal.get(Calendar.MONTH), cal.get(Calendar.DATE));
@@ -89,5 +77,63 @@ public class DateCustomDialogActivity extends AppCompatActivity {
                 tpd.show();
             }
         });
+
+        btn_bp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                show(btn_bp);
+            }
+        });
+
+        btn_hp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                show(btn_hp);
+            }
+        });
+
+        btn_mp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                show(btn_mp);
+            }
+        });
+    }
+
+    private void show(final Button btn) {
+        final Dialog dialog = new Dialog(DateCustomDialogActivity.this);
+
+        dialog.setTitle("Set BP");
+        dialog.setContentView(R.layout.dialog);
+
+        Button b1 = dialog.findViewById(R.id.button1);
+        Button b2 = dialog.findViewById(R.id.button1);
+
+        final NumberPicker np = dialog.findViewById(R.id.numberPicker1);
+        np.setMaxValue(100);
+        np.setMinValue(0);
+        np.setWrapSelectorWheel(true);
+        np.setOnValueChangedListener(this);
+
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                btn.setText(String.valueOf(np.getValue()));
+                dialog.dismiss();
+            }
+        });
+
+        b2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dialog.dismiss();
+            }
+        });
+        dialog.show();
+    }
+
+    @Override
+    public void onValueChange(NumberPicker numberPicker, int i, int i1) {
+
     }
 }
