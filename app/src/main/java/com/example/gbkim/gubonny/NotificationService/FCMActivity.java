2ae8152c02dda9826b4662739fbb5c56702d9d5a
gbkim <kgb8594@gmail.com>
2018-03-14 15:36:02 +0900

    Renew FCM Notification

--- app/src/main/java/com/example/gbkim/gubonny/NotificationService/FCMActivity.java
+++ app/src/main/java/com/example/gbkim/gubonny/NotificationService/FCMActivity.java
@@ -6,6 +6,7 @@ import android.util.Log;
 
 import com.example.gbkim.gubonny.R;
 import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.messaging.FirebaseMessaging;
 
 public class FCMActivity extends AppCompatActivity {
 
@@ -14,8 +15,12 @@ public class FCMActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fcm);
 
-        String token = FirebaseInstanceId.getInstance().getToken();
-        Log.d("FCM_Token", token);
+        //추가한 라인
+        FirebaseMessaging.getInstance().subscribeToTopic("news");
+        FirebaseInstanceId.getInstance().getToken();
+
+//        String token = FirebaseInstanceId.getInstance().getToken();
+//        Log.d("FCM_Token", token);
 
 
     }
